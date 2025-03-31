2b86f46be17b915518bcba186c3a7d9d48dc4265
farhansayyed165 <farhansayyed1656@gmail.com>
2025-03-31 21:55:53 +0530

    fixed api

--- queries.js
+++ queries.js
@@ -1,21 +1,33 @@
-require("dotenv").config()
-const Pool = require("pg").Pool;
+// require("dotenv").config()
+// const Pool = require("pg").Pool;
 
-// const knex = require('knex')({
-//     client: 'pg',
-//     connection: {
-//       host : '127.0.0.1',
-//       port : 5432,
-//       user : 'wp',
-//       password : 'farhan165',
-//       database : 'VanLife'
-//     }
-//   });
+// // const knex = require('knex')({
+// //     client: 'pg',
+// //     connection: {
+// //       host : '127.0.0.1',
+// //       port : 5432,
+// //       user : 'wp',
+// //       password : 'farhan165',
+// //       database : 'VanLife'
+// //     }
+// //   });
   
 
+// const pool = new Pool({
+//     connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+// }) ;
+
+
+require('dotenv').config();  // Ensure dotenv is loaded
+
+const { Pool } = require('pg');
+
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-}) ;
+    connectionString: process.env.POSTGRES_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
 
 module.exports = {pool}
-
